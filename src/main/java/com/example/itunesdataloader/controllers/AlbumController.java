2c654ead649b0edf3c9a72d30ab78ee782135e9f
niko1ai-krasavin <kosn91@mail.ru>
2021-11-08 22:39:54 +0200

    Auto-downloading is added

--- src/main/java/com/example/itunesdataloader/controllers/AlbumController.java
+++ src/main/java/com/example/itunesdataloader/controllers/AlbumController.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.itunesdataloader.entities.Song;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,6 +30,8 @@ public class AlbumController {
     @Autowired
     private final AlbumDTOMapper albumDTOMapper;
 
+    @Autowired
+    private final ItunesDataLoaderController itunesDataLoaderController;
 
     @GetMapping()
     public String getAllAlbums(Model model) {
@@ -51,6 +54,11 @@ public class AlbumController {
         List<AlbumDTO> albumDTOList = new ArrayList<>();
         for (Album item : albumService.findAllByArtistId(artistId)) {
             albumDTOList.add(albumDTOMapper.getAlbumDTO(item));
+        }
+        if (albumDTOList.size() == 0) {
+            itunesDataLoaderController.getSongsFromITunes(artistId.toString(), model);
+            for (Album item : albumService.findAllByArtistId(artistId))
+                albumDTOList.add(albumDTOMapper.getAlbumDTO(item));
         }
         model.addAttribute("albumResponse", albumDTOList);
         return "albums";
